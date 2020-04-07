fac597605e9be91d9e5afbea16b4da534e9a0373
TheBlackg0d <50238892+TheBlackg0d@users.noreply.github.com>
2020-04-07 14:16:15 -0400

    wip

--- FriendZone/FriendZoneWeb/App_Start/BundleConfig.cs
+++ FriendZone/FriendZoneWeb/App_Start/BundleConfig.cs
@@ -22,11 +22,10 @@ namespace FriendZoneWeb
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
-            bundles.Add(new StyleBundle("~/Content/bootstrapcss").Include(
-                        "~/Content/bootstrap.min.css",
-                        "~/Content/bootstrap.css",
-                        "~/Content/font-awesome.css")); 
-                     
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css"));
+
         }
     }
 }
